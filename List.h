37ce84df8f4b845dc41ae8fd3280aa4edce94e29
h1gro <komarov.aa@phystech.edu>
2024-11-07 22:28:40 +0300

    version with push and pop

---
+++ List.h
@@ -0,0 +1,29 @@
+#ifndef LIST
+#define LIST
+
+#include <stdio.h>
+
+static const char* DUMP       = "dump.txt";
+static const char* DUMP_HTML  = "Dump.html";
+
+const size_t LIST_SIZE    = 10;
+const int INVALID_ADDR    = -1;
+const int POISON          = -153;
+
+struct list_t
+{
+    FILE*       dump;
+    FILE*       dump_html;
+    int*        data;
+    int*        next;
+    int*        prev;
+    int         free;
+    int         anchor;
+    int         number_elements;
+};
+
+void ListCtor(struct list_t* list);
+void ListDtor(struct list_t* list);
+void FillingNextPrevPoison(struct list_t* list);
+
+#endif
